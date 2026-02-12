bf7b541354f6b0760ec0c1c751188b5a8af4b9b4
junjie <junegiftsonsindac@gmail.com>
2026-02-12 22:42:58 +1030

    update 2

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const noBtn = document.querySelector(".no-btn");
 const yesBtn = document.querySelector(".btn[alt='Yes']");
 
 const title = document.getElementById("letter-title");
-const catImg = document.getElementById("letter-cat");
+const dogImg = document.getElementById("letter-cat");
 const buttons = document.getElementById("letter-buttons");
 const finalText = document.getElementById("final-text");
 
@@ -62,7 +62,7 @@ noBtn.addEventListener("mouseover", () => {
 yesBtn.addEventListener("click", () => {
     title.textContent = "Yaaaaay thank you!";
 
-    catImg.src = "golden-retriever-dog.gif";
+    dogImg.src = "golden-retriever-dog.gif";
 
     document.querySelector(".letter-window").classList.add("final");
 
